8a932ec7637ef3550dc06f8d704ebc7fd68f849d
mcleancapital <ianmclean@mcleancapital.ca>
2025-01-04 08:57:04 -0500

    Update download_sp500_eps.py

--- scripts/download_sp500_eps.py
+++ scripts/download_sp500_eps.py
@@ -12,7 +12,8 @@ def download_excel():
     filepath = os.path.join(SAVE_DIR, filename)
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Referer": "https://www.spglobal.com/",
     }
 
     try:
